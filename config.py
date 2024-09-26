20491db225d30ba3cf4913889c62c8008ed39ed5
Muhsin <142356973+MuhsinMet@users.noreply.github.com>
2024-09-26 08:57:33 +0100

    Update config.py
    chage of folder paths

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ import os
 import numpy as np
 
 config = {
-    'dir_data_processed': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED',
-    'dir_data_raw': '/mnt/datawaha/hyex/your_project/DATA_RAW',
-    'dir_output': '/mnt/datawaha/hyex/your_project/OUTPUT',
+    'dir_data_processed': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED',
+    'dir_data_raw': '/mnt/datawaha/hyex/msn/GWPM/DATA_RAW',
+    'dir_output': '/mnt/datawaha/hyex/msn/GWPM/OUTPUT',
     'dir_temp': '/tmp',
     'parameters': ['Temp', 'P', 'RelHum', 'wind'],
 }
@@ -13,26 +13,26 @@ config = {
 models = {
     'GEFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/GEFS',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/GEFS',
     },
     'ICON': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ICON',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ICON',
     },
     'ECMWF_IFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ECMWF_IFS',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ECMWF_IFS',
     },
     'ECMWF_AIFS': {
         'predictors': ['Temp', 'P',  'wind'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ECMWF_AIFS',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ECMWF_AIFS',
     },
     'ERA5': {
         'predictors': ['Temp', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ERA5',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ERA5',
     },
     'MSWEP': {
         'predictors': ['P'],
-        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/MSWEP',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/MSWEP',
     }
 }
