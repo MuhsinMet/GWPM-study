4ee528351dbcf176f151dd3309ec4ab8457e01a9
Muhsin <142356973+MuhsinMet@users.noreply.github.com>
2024-09-26 11:40:18 +0100

    Updated paths.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 config = {
-    'dir_data_processed': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED',
+    'dir_data_processed': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/',
     'dir_data_raw': '/mnt/datawaha/hyex/msn/GWPM/DATA_RAW',
     'dir_output': '/mnt/datawaha/hyex/msn/GWPM/OUTPUT',
     'dir_temp': '/tmp',
@@ -13,26 +13,26 @@ config = {
 models = {
     'GEFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/GEFS',
+        'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/GEFS/',
     },
     'ICON': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ICON',
+        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ICON',
     },
     'ECMWF_IFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ECMWF_IFS',
+        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ECMWF_IFS_open_ensemble_forecasts',
     },
     'ECMWF_AIFS': {
         'predictors': ['Temp', 'P',  'wind'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ECMWF_AIFS',
+        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ECMWF_AIFS_open_ensemble_forecasts',
     },
     'ERA5': {
         'predictors': ['Temp', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/ERA5',
+        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ERA5_HRES',
     },
     'MSWEP': {
         'predictors': ['P'],
-        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/MSWEP',
+        'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/MSWEP_V300',
     }
 }
