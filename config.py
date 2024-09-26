3f9d9125955b7a87144539814fd8de5cc9aded61
Muhsin <142356973+MuhsinMet@users.noreply.github.com>
2024-09-26 10:43:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+import os
+import numpy as np
+
+config = {
+    'dir_data_processed': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED',
+    'dir_data_raw': '/mnt/datawaha/hyex/your_project/DATA_RAW',
+    'dir_output': '/mnt/datawaha/hyex/your_project/OUTPUT',
+    'dir_temp': '/tmp',
+    'parameters': ['Temp', 'P', 'RelHum', 'wind'],
+}
+
+models = {
+    'GEFS': {
+        'predictors': ['Temp', 'P', 'RelHum', 'wind'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/GEFS',
+    },
+    'ICON': {
+        'predictors': ['Temp', 'P', 'RelHum', 'wind'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ICON',
+    },
+    'ECMWF_IFS': {
+        'predictors': ['Temp', 'P', 'RelHum', 'wind'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ECMWF_IFS',
+    },
+    'ECMWF_AIFS': {
+        'predictors': ['Temp', 'P',  'wind'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ECMWF_AIFS',
+    },
+    'ERA5': {
+        'predictors': ['Temp', 'RelHum', 'wind'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/ERA5',
+    },
+    'MSWEP': {
+        'predictors': ['P'],
+        'data_path': '/mnt/datawaha/hyex/your_project/DATA_PROCESSED/MSWEP',
+    }
+}
