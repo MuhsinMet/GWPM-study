2eb74318bd3e404ed9d4cc8b36d8d5ecd4a32327
Muhsin <142356973+MuhsinMet@users.noreply.github.com>
2024-09-29 08:37:47 +0100

    Update config.py
    MSWEP and ERA5 under Reference data

--- config.py
+++ config.py
@@ -8,41 +8,47 @@ config = {
     'dir_output': '/mnt/datawaha/hyex/msn/GWPM/OUTPUT',
     'dir_temp': '/tmp',
     'parameters': ['Temp', 'P', 'RelHum', 'wind'],
+    'forecast_dates': ['20240816_00'],  # This date can be changed as needed
 }
 
 models = {
     'GEFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
         'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/GEFS',
-        'file_path_template': '{base_path}/{parameter}/{date_time}/{ensemble}/Daily/{file_name}.nc',
-        'ensemble_format': '01'  # Example for accessing the 1st ensemble member
+        'file_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/GEFS/Temp/20240816_00/01/Daily/2024230.nc',
+        'variable_name': 'air_temperature'
     },
     'ICON': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ICON',
-        'file_path_template': '{base_path}/{parameter}/{date_time}/Daily/{file_name}.nc'
+        'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ICON',
+        'file_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ICON/Temp/20240816_00/Daily/2024230.nc',
+        'variable_name': 'air_temperature'
     },
     'ECMWF_IFS': {
         'predictors': ['Temp', 'P', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ECMWF_IFS_open_ensemble_forecasts',
-        'file_path_template': '{base_path}/{parameter}/{date_time}/{ensemble}/Daily/{file_name}.nc',
-        'ensemble_format': '001'  # Example for accessing the 1st ensemble member
+        'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ECMWF_IFS_open_ensemble_forecasts',
+        'file_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ECMWF_IFS_open_ensemble_forecasts/Temp/20240816_00/001/Daily/2024230.nc',
+        'variable_name': 'air_temperature'
     },
     'ECMWF_AIFS': {
         'predictors': ['Temp', 'P', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ECMWF_AIFS_open_ensemble_forecasts',
-        'file_path_template': '{base_path}/{parameter}/{date_time}/{ensemble}/Daily/{file_name}.nc',
-        'ensemble_format': '001'  # Example for accessing the 1st ensemble member
-    },
+        'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ECMWF_AIFS_open_ensemble_forecasts',
+        'file_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ECMWF_AIFS_open_ensemble_forecasts/Temp/20240816_00/001/Daily/2024230.nc',
+        'variable_name': 'air_temperature'
+    }
+}
+
+# Reference data (ERA5 and MSWEP)
+reference_data = {
     'ERA5': {
-        'predictors': ['Temp', 'RelHum', 'wind'],
-        'data_path': '/mnt/datawaha/hyex/becke/DATA_PROCESSED/ERA5_HRES',
-        'file_path_template': '{base_path}/{parameter}/Daily/{file_name}.nc'
+        'data_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ERA5_HRES',
+        'file_path': '/mnt/datawaha/hyex/beckhe/DATA_PROCESSED/ERA5_HRES/Temp/Daily/2024230.nc',
+        'variable_name': 'air_temperature'
     },
     'MSWEP': {
-        'predictors': ['P'],
         'data_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/MSWEP_V300',
-        'file_path_template': '{base_path}/{parameter}/Daily/{file_name}.nc'
+        'file_path': '/mnt/datawaha/hyex/msn/GWPM/DATA_PROCESSED/MSWEP_V300/P/Daily/2024230.nc',
+        'variable_name': 'precipitation'
     }
 }
 
